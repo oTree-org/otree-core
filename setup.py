5f5104fd32dec20c1b2fb8272cfde9fa8e67c635
wickens <c.wickens@gmail.com>
2013-11-20 22:20:46 +0800

    took manage.py and setup.py out of .gitignore

---
+++ setup.py
@@ -0,0 +1,50 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-ptree',
+    version='0.1.41',
+    packages=['ptree',
+              'ptree.models',
+              'ptree.management',
+              'ptree.stuff',
+              'ptree.templatetags',
+              'ptree.views',
+              'ptree.questionnaires',
+              'ptree.questionnaires.life_orientation_test',
+              'ptree.management.commands'],
+    include_package_data=True,
+    license='MIT License',
+    description='pTree is a Django toolset that makes it easy to create and administer social science experiments to online participants.',
+    long_description=README,
+    url='http://ptree.org/',
+    author='Chris Wickens',
+    author_email='c.wickens+ptree@googlemail.com',
+    install_requires = ['boto==2.13.3',
+                        'django-crispy-forms==1.4.0',
+                        'Django == 1.6.0',
+                        'django-data-exports==0.6',
+                        'django-vanilla-views==1.0.2',
+                        'Babel==1.3',
+
+                        ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # replace these appropriately if you are using Python 3
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    scripts = ['bin/ptree'],
+)
