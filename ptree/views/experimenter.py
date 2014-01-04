aaffbe1d9dcb22e1a1bf6bf07322d50d313da405
wickens <c.wickens@gmail.com>
2014-01-04 17:00:40 +0100

    .

--- ptree/views/experimenter.py
+++ ptree/views/experimenter.py
@@ -90,7 +90,6 @@ class ExperimenterSequenceMixin(ExperimenterMixin):
         context.update(super(ExperimenterSequenceMixin, self).get_context_data(**kwargs))
         context.update(self.variables_for_template())
 
-        print context
         return context
 
     def get_form(self, data=None, files=None, **kwargs):
