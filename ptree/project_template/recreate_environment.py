d802a11cd6d33a332fb695d76a11d483e7ecff7d
wickens <c.wickens@gmail.com>
2014-01-14 23:11:02 -0800

    updated app & project template

--- ptree/project_template/recreate_environment.py
+++ ptree/project_template/recreate_environment.py
@@ -11,30 +11,35 @@ import copy_ptree
 import commit_and_push
 import sys
 
+# use this when you've made changes to the code (on the dev site) that affected the database schema.
+# it will drop the dev DB and recreate it, thus providing a blank slate.
+
+import sys, os
+import commit_and_push
+
+# replace the below app names with your heroku app names
+heroku_apps = {'dev': 'heroku-app-name-dev',
+               'staging': 'heroku-app-name-staging',
+               'production': 'heroku-app-name-production'}
+
 def main():
     environment = sys.argv[1]
-    num_participants = 50
-    app_name = 'your_app_name_here'
-
-    syncdb = 'python manage.py syncdb'
-    create_objects = 'python manage.py create_objects --app_name={} --participants={}'.format(app_name,
-                                                                                   num_participants)
+    syncdb = 'python manage.py syncdb --traceback'
+    create_session = 'python manage.py create_session'
 
     if environment == 'local':
-        open('ptree_experiments/db.sqlite3', 'w').write('')
+        open('{{ project_name }}/db.sqlite3', 'w').write('')
         os.system(syncdb)
-        os.system(create_objects)
+        os.system(create_session)
+
         # then launch from PyCharm
     else: # heroku
-        if environment == 'production':
-            confirmed = raw_input('Enter "y" to confirm deletion of your production database').lower() == 'y'
-            if not confirmed:
-                sys.exit(0)
 
-        # replace the below app names with your heroku app names
-        heroku_apps = {'dev': 'heroku-app-name-dev',
-                       'staging': 'heroku-app-name-staging',
-                       'production': 'heroku-app-name-production'}
+        if environment == 'production':
+            confirmed = raw_input('Enter "y" if you are sure you want to reset the production database.').lower() == 'y'
+            if not confirmed:
+                print 'exit.'
+                return
 
         reset_db = 'heroku pg:reset DATABASE --confirm {}'.format(heroku_apps[environment])
         os.system(reset_db)
@@ -42,11 +47,17 @@ def main():
         commit_and_push.run(environment)
 
         heroku_run_command = 'heroku run {} --remote {}'
+
         syncdb = heroku_run_command.format(syncdb, environment)
-        create_objects = heroku_run_command.format(create_objects, environment)
+        create_session = heroku_run_command.format(create_session, environment)
 
         os.system(syncdb)
-        os.system(create_objects)
+        print create_session
+        os.system(create_session)
 
 if __name__ == '__main__':
     main()
+
+# creating objects on production
+# DOES NOT RESET DATABASE
+# heroku run python manage.py create_session --remote production
