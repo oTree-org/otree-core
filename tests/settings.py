8649aae95bc5bedcd7c219101194a28bd93adf86
Gregor Müllegger <gregor@muellegger.de>
2014-09-22 16:35:27 +0200

    Using a dedicated settings file for the otree core tests. You can run the tests now by calling 'make test' in the otree-core root directory.

---
+++ tests/settings.py
@@ -0,0 +1,30 @@
+DEBUG = False
+
+
+ADMIN_USERNAME = 'admin'
+ADMIN_PASSWORD = 'otree'
+SECRET_KEY = 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+
+INSTALLED_APPS = (
+    'otree',
+    'raven.contrib.django.raven_compat',
+)
+
+
+CURRENCY_CODE = 'USD'
+LANGUAGE_CODE = 'en-us'
+
+
+INSTALLED_OTREE_APPS = [
+    'trust',
+    'lab_results',
+]
