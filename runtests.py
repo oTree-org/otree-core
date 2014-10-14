242c5d3f9870490f3a4ccbb1751ab9e2f3854c4b
Gregor Müllegger <gregor@muellegger.de>
2014-10-14 23:16:00 +0200

    Making runtests.py work by adding session module and using new settings format. Also not using the 'test' management command by name, but importing django's command directly.

--- runtests.py
+++ runtests.py
@@ -9,17 +9,17 @@ sys.path.insert(0, base_path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
 
 
-from django.core.management import execute_from_command_line
-
-
 default_test_apps = [
     'tests',
 ]
 
 
 def runtests(*args):
+    from django.core.management.commands.test import Command
+
+    test_command = Command()
     test_apps = list(args or default_test_apps)
-    execute_from_command_line([sys.argv[0], 'test', '--verbosity=1'] + test_apps)
+    test_command.execute(verbosity=1, *test_apps)
 
 
 if __name__ == '__main__':
---
+++ tests/session.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from otree.session import SessionType
+
+
+def session_types():
+    return []
+
+
+def show_on_demo_page(session_type_name):
+    return True
+
+
+demo_page_intro_text = """
+<ul>
+    <li><a href="https://github.com/oTree-org/otree" target="_blank">Source code</a> for the below games.</li>
+    <li><a href="http://www.otree.org/" target="_blank">oTree homepage</a>.</li>
+</ul>
+<p>
+Below are various games implemented with oTree. These games are all open source,
+and you can modify them as you wish to create your own variations. Click one to learn more and play.
+</p>
+"""
