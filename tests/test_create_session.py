1cc9c55263d804fdf027f754c967233d057943a2
Gregor Müllegger <gregor@muellegger.de>
2014-10-30 21:34:11 +0100

    Using capture_stdout context manager for easing tests.

--- tests/test_create_session.py
+++ tests/test_create_session.py
@@ -1,34 +1,23 @@
-import sys
-from StringIO import StringIO
-
 from django.test import TestCase
 from django.core.management import call_command
 
 from otree.models import Session
 from tests.simple_game.models import Subsession, Player
+from tests.utils import capture_stdout
 
 
 class TestCreateSessionsCommand(TestCase):
-    def setUp(self):
-        self._original_stdout = sys.stdout
-        self.stdout = sys.stdout = StringIO()
-
-    def tearDown(self):
-        sys.stdout = self._original_stdout
-
-    def get_output(self):
-        self.stdout.seek(0)
-        return self.stdout.read()
-
     def test_create_sessions_output(self):
-        call_command('create_sessions', 'simple_game', 1)
-        output = self.get_output()
+        with capture_stdout() as output_stream:
+            call_command('create_sessions', 'simple_game', 1)
+        output = output_stream.read()
 
         self.assertTrue('tests.simple_game' in output)
 
     def test_create_two_sessions_output(self):
-        call_command('create_sessions', 'simple_game', 2)
-        output = self.get_output()
+        with capture_stdout() as output_stream:
+            call_command('create_sessions', 'simple_game', 2)
+        output = output_stream.read()
 
         lines = output.strip().splitlines()
         self.assertEqual(len(lines), 3)
@@ -37,7 +26,8 @@ class TestCreateSessionsCommand(TestCase):
         self.assertTrue('tests.simple_game' in lines[2])
 
     def test_create_one_session(self):
-        call_command('create_sessions', 'simple_game', 1)
+        with capture_stdout():
+            call_command('create_sessions', 'simple_game', 1)
 
         self.assertEqual(Session.objects.count(), 1)
         session = Session.objects.get()
---
+++ tests/utils.py
@@ -0,0 +1,14 @@
+import contextlib
+import sys
+from StringIO import StringIO
+
+
+@contextlib.contextmanager
+def capture_stdout(target=None):
+    original = sys.stdout
+    if target is None:
+        target = StringIO()
+    sys.stdout = target
+    yield target
+    target.seek(0)
+    sys.stdout = original
