36cc64404cb39b85a4de18faa57bb0f6be65e46d
Juan B Cabral <jbc.develop@gmail.com>
2014-11-25 01:58:08 -0300

    fixed dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,39 @@
 import os
 import sys
+
 from setuptools import setup, find_packages
 
+
 README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+
+REQUIREMENTS = [
+    'Babel>=1.3',
+    'Django>=1.7.1',
+    'Pillow>=2.6.1',
+    'coverage>=3.7.1',
+    'dj-static>=0.0.6',
+    'django-countries>=3.0.1',
+    'django-easymoney>=0.5',
+    'django-extensions>=1.4.6',
+    'django-ptree-extra-views>=0.6.3',
+    'django-floppyforms>=1.2.0',
+    'django-inspect-model>=0.7',
+    'django-ptree-mturk>=1.0.1',
+    'django-save-the-change>=1.0.0',
+    'django-vanilla-views>=1.0.2',
+    'handy>=0.3',
+    'pytz>=2013.9',
+    'raven>=5.1.1',
+    'selenium>=2.41.0',
+    'xmltodict>=0.9.0'
+]
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-version='0.2.227'
+version='0.2.228'
+
 
 if sys.argv[-1] == 'publish':
 
@@ -31,34 +57,20 @@ setup(
     version=version,
     include_package_data=True,
     license='MIT License',
-    # this was not working right. did not exclude otree.app_template._builtin for some reason.
-    # so instead i use recursive-exclude in MANIFEST.in
+
+    # this was not working right. did not exclude
+    # otree.app_template._builtin for some reason. so instead i use
+    # recursive-exclude in MANIFEST.in
     packages=find_packages(),
-    description='oTree is a toolset that makes it easy to create and administer web-based social science experiments.',
+    description=(
+        'oTree is a toolset that makes it easy to create and '
+        'administer web-based social science experiments.'
+    ),
     long_description=README,
     url='http://otree.org/',
     author='C. Wickens',
     author_email='c.wickens+otree@googlemail.com',
-    install_requires = [
-        'django-floppyforms==1.2.0',
-        'Django==1.7.1',
-        'django-vanilla-views==1.0.2',
-        'Babel==1.3',
-        'raven==3.5.2',
-        'django-inspect-model',
-        'django-ptree-extra-views',
-        'dj-static==0.0.6',
-        'selenium==2.41.0',
-        'xmltodict==0.9.0',
-        'django-ptree-mturk',
-        'django-extensions',
-        'django-save-the-change==1.0.0',
-        'pytz==2013.9',
-        'coverage==3.7.1',
-        'django-easymoney==0.5',
-        'handy==0.3',
-        'Pillow',
-    ],
+    install_requires = REQUIREMENTS,
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
