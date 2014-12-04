14f01ac77cca1cf4f6c9e0096ae2a758289fe188
wickens <c.wickens@gmail.com>
2014-12-04 22:54:58 +0900

    removing stuff from otree.management.__init__, because as of django 1.7 i think it belongs in otree.apps

--- otree/apps.py
+++ otree/apps.py
@@ -2,8 +2,33 @@ from django.apps import AppConfig, apps
 from django.conf import settings
 from django.db.models import signals
 from django.utils.translation import ugettext_lazy as _
-from .management import create_default_superuser, create_singleton_objects
 from . import checks
+import logging
+
+logger = logging.getLogger('otree')
+
+from otree.session.models import StubModel, GlobalSingleton
+
+def create_default_superuser(sender, **kwargs):
+    """
+    Creates our default superuser.
+    """
+    User = apps.get_model('auth.User')
+    username = settings.ADMIN_USERNAME
+    password = settings.ADMIN_PASSWORD
+    if not User.objects.filter(username=username).exists():
+        logger.info(
+            'Creating default superuser. '
+            'Username: {}'.format(username))
+        assert User.objects.create_superuser(username, email='', password=password)
+    else:
+        logger.debug('Default superuser already exists.')
+
+
+def create_singleton_objects(sender, **kwargs):
+    for ModelClass in [StubModel, GlobalSingleton]:
+        # if it doesn't already exist, create one.
+        ModelClass.objects.get_or_create()
 
 
 class OtreeConfig(AppConfig):
--- otree/management/__init__.py
+++ otree/management/__init__.py
@@ -1,30 +1,3 @@
-import logging
-from django.apps import apps
-from django.conf import settings
-from otree.session.models import StubModel, GlobalSingleton
 
 
-logger = logging.getLogger('otree')
 
-
-def create_default_superuser(sender, **kwargs):
-    """
-    Creates our default superuser.
-    """
-    User = apps.get_model('auth.User')
-    username = settings.ADMIN_USERNAME
-    password = settings.ADMIN_PASSWORD
-    email = getattr(settings, 'ADMIN_EMAIL', '')
-    if not User.objects.filter(username=username).exists():
-        logger.info(
-            'Creating default superuser. '
-            'Username: {} Email: {}'.format(username, email))
-        assert User.objects.create_superuser(username, email, password)
-    else:
-        logger.debug('Default superuser already exists.')
-
-
-def create_singleton_objects(sender, **kwargs):
-    for ModelClass in [StubModel, GlobalSingleton]:
-        # if it doesn't already exist, create one.
-        ModelClass.objects.get_or_create()
