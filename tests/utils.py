d7b3ef32f74ff3f36203a5dea81fe85bcfc3471f
Gregor Müllegger <gregor@muellegger.de>
2014-12-11 09:06:08 +0100

    Adding cd and add_path contextmanagers for tests.

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,20 @@
 import contextlib
+import os
 import sys
 from StringIO import StringIO
+
+
+@contextlib.contextmanager
+def add_path(path):
+    """
+    ::
+
+        with add_path(new_sys_path):
+            import strange_module
+    """
+    sys.path.insert(0, path)
+    yield
+    sys.path.pop(sys.path.index(path))
 
 
 @contextlib.contextmanager
@@ -12,3 +26,17 @@ def capture_stdout(target=None):
     yield target
     target.seek(0)
     sys.stdout = original
+
+
+@contextlib.contextmanager
+def cd(directory):
+    """
+    ::
+
+        with cd(new_cwd):
+            os.walk('.')
+    """
+    old_path = os.getcwd()
+    os.chdir(directory)
+    yield
+    os.chdir(old_path)
