2da510bcc2a2e6611c9249cf0728d06b70b5465b
Juan B Cabral <jbc.develop@gmail.com>
2014-12-24 04:11:02 -0300

    fix pep8 in otree.views.ajax_change_list

--- otree/views/ajax_change_list.py
+++ otree/views/ajax_change_list.py
@@ -1,12 +1,19 @@
-__doc__ = """This module contains views for ajax calls from ajax change list pages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This module contains views for ajax calls from ajax change list pages
+
 """
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
 import urllib
 import cgi
-import json
+
 from django.utils import six
-
-from django.conf import settings
-
 from django.contrib import admin
 from django.contrib.admin import options
 from django.http import HttpResponse
@@ -15,15 +22,16 @@ from otree.templatetags import ajax_admin_list
 
 
 def get_cl(request, model_admin):
-    from django.contrib.admin.views.main import ERROR_FLAG
-    opts = model_admin.model._meta
-    app_label = opts.app_label
+    # from django.contrib.admin.views.main import ERROR_FLAG
+
     # TODO
-    #if not model_admin.has_change_permission(request, None):
+    # if not model_admin.has_change_permission(request, None):
     #    raise PermissionDenied
 
     list_display = model_admin.get_list_display(request)
-    list_display_links = model_admin.get_list_display_links(request, list_display)
+    list_display_links = model_admin.get_list_display_links(
+        request, list_display
+    )
     list_filter = model_admin.get_list_filter(request)
 
     # Check actions to see if any are available on this changelist
@@ -34,11 +42,13 @@ def get_cl(request, model_admin):
 
     ChangeList = model_admin.get_changelist(request)
     try:
-        cl = ChangeList(request, model_admin.model, list_display,
+        cl = ChangeList(
+            request, model_admin.model, list_display,
             list_display_links, list_filter, model_admin.date_hierarchy,
             model_admin.search_fields, model_admin.list_select_related,
-            model_admin.list_per_page, model_admin.list_max_show_all, model_admin.list_editable,
-            model_admin)
+            model_admin.list_per_page, model_admin.list_max_show_all,
+            model_admin.list_editable, model_admin
+        )
     except options.IncorrectLookupParameters:
         pass
         # Wacky lookup parameters were given, so redirect to the main
@@ -48,21 +58,21 @@ def get_cl(request, model_admin):
         # something is screwed up with the database, so display an error
         # page.
         # TODO: handle this error
-        #if ERROR_FLAG in request.GET.keys():
+        # if ERROR_FLAG in request.GET.keys():
         #    return SimpleTemplateResponse('admin/invalid_setup.html', {
         #        'title': _('Database error'),
         #    })
-        #return HttpResponseRedirect(request.path + '?' + ERROR_FLAG + '=1')
+        # return HttpResponseRedirect(request.path + '?' + ERROR_FLAG + '=1')
 
     # If we're allowing changelist editing, we need to construct a formset
     # for the changelist given all the fields to be edited. Then we'll
     # use the formset to validate/process POSTed data.
-    formset = cl.formset = None
+    cl.formset = None
 
     # Handle GET -- construct a formset for display.
     if cl.list_editable:
         FormSet = model_admin.get_changelist_formset(request)
-        formset = cl.formset = FormSet(queryset=cl.result_list)
+        cl.formset = FormSet(queryset=cl.result_list)
 
     return cl
 
@@ -72,9 +82,11 @@ class FakeResolverMatch:
         self.app_name = app_name
         self.url_name = url_name
 
+
 class FakeGET(dict):
     def urlencode(self):
         return urllib.urlencode(self)
+
 
 class FakeRequest:
     def __init__(self, resolver_match, get_params, user):
@@ -89,19 +101,24 @@ class FakeRequest:
 from otree.adminlib import autodiscover
 autodiscover()
 url_name2model_admin = None
+
+
 def get_model_admin_from(url_name):
     # TODO error handling
     global url_name2model_admin
     if url_name2model_admin is None:
         url_name2model_admin = {}
         for model, model_admin in six.iteritems(admin.site._registry):
-            url_name2model_admin['%s_%s_changelist' % (model._meta.app_label, model._meta.model_name)] = model_admin
+            url_name2model_admin[
+                '{}_{}_changelist'.format(model._meta.app_label,
+                                          model._meta.model_name)
+            ] = model_admin
     return url_name2model_admin[url_name]
 
 
 # TODO: admin priveledge only
 def ajax_otree_change_list_results(request):
-    #TODO
+    # TODO
     url_name = request.GET["url_name"]
     get_params_str = request.GET["get_params"]
     parsed_qs = cgi.parse_qs(get_params_str, keep_blank_values=True)
