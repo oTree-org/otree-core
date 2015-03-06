199645da16ba4a2786af0961a1774b369bc4b47e
Gregor Müllegger <gregor@muellegger.de>
2015-03-06 15:03:21 +0100

    Print a warning in the HTML if not all formfields are rendered on the page.

--- otree/templatetags/otree_forms.py
+++ otree/templatetags/otree_forms.py
@@ -1,6 +1,7 @@
 import sys
 
 from django.db import models
+from django.template import Context
 from django.template import Node
 from django.template import TemplateSyntaxError
 from django.template import Variable
@@ -8,6 +9,72 @@ from django.template import VariableDoesNotExist
 from django.template.base import token_kwargs
 from django.template.loader import get_template
 from django.utils import six
+
+import floppyforms.templatetags.floppyforms as floppyforms_templatetags
+
+
+def mark_field_as_rendered(context, bound_field):
+    if not hasattr(context, '_rendered_fields'):
+        context._rendered_fields = set()
+    context._rendered_fields.add(bound_field.field)
+
+
+class FormNode(floppyforms_templatetags.FormNode):
+    def __init__(self, *args, **kwargs):
+        super(FormNode, self).__init__(*args, **kwargs)
+        self.formfield_is_missing_template = get_template(
+            'otree/forms/_formfield_is_missing_error.html')
+
+    def get_form_instance(self, context):
+        extra_context = self.get_extra_context(context)
+        form = extra_context[self.single_template_var]
+        return form
+
+    def get_error_html(self, form, missing_fields):
+        return self.formfield_is_missing_template.render(Context({
+            'form': form,
+            'missing_fields': missing_fields,
+        }))
+
+    def get_rendered_fields(self, context):
+        return getattr(context, '_rendered_fields', [])
+
+    def render(self, context):
+        result = super(FormNode, self).render(context)
+        form = self.get_form_instance(context)
+        if form is not None:
+            missing_fields = []
+            for field_name, field in form.fields.items():
+                if field not in self.get_rendered_fields(context):
+                    missing_fields.append((field_name, field))
+            if missing_fields:
+                result += self.get_error_html(form, missing_fields)
+        return result
+
+
+class MarkFieldAsRenderedNode(Node):
+    def __init__(self, tagname, field_variable):
+        self.tagname = tagname
+        self.field_variable = field_variable
+
+    def mark_field_as_rendered(self, context, bound_field):
+        mark_field_as_rendered(context, bound_field)
+
+    def render(self, context):
+        bound_field = self.field_variable.resolve(context)
+        self.mark_field_as_rendered(context, bound_field)
+        return ''
+
+    @classmethod
+    def parse(cls, parser, token):
+        bits = token.split_contents()
+        tagname = bits.pop(0)
+        if len(bits) != 1:
+            raise TemplateSyntaxError(
+                '{tagname!r} takes exactly one argument.'.format(
+                    tagname=tagname))
+        field_variable = Variable(bits.pop(0))
+        return cls(tagname, field_variable)
 
 
 class FormFieldNode(Node):
@@ -95,6 +162,7 @@ class FormFieldNode(Node):
             rendered = self.default_template.render(context)
         finally:
             context.pop()
+        mark_field_as_rendered(context, extra_context['bound_field'])
         return rendered
 
     @classmethod
