f12f30e68e654f3e01e1af1135e58b9b0f81f4c3
wickens <c.wickens@gmail.com>
2015-03-17 23:12:45 +0900

    bump

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ REQUIREMENTS = [
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-version = '0.2.276'
+version = '0.2.277'
 
 
 if sys.argv[-1] == 'publish':
