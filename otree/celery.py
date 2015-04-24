a9398235a1277fbca1803c2c6299f86e7ba366a6
Gregor Müllegger <gregor@muellegger.de>
2015-04-24 17:13:55 +0200

    Trying new django-celery setup.

---
+++ otree/celery.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+
+def setup_celery_app():
+    from celery import Celery
+    from django.conf import settings
+
+    app = Celery('otree')
+
+    # Using a string here means the worker will not have to
+    # pickle the object when using Windows.
+    app.config_from_object('django.conf:settings')
+    app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+    return app
