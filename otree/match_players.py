2d4e8bea9d7e8778e3b15a60e56a3cec311ebd63
Juan B Cabral <jbc.develop@gmail.com>
2015-05-10 18:10:39 -0300

    end round robin

--- otree/match_players.py
+++ otree/match_players.py
@@ -11,6 +11,7 @@
 
 import functools
 import itertools
+import collections
 
 
 # =============================================================================
@@ -25,7 +26,32 @@ MATCHS = {}
 # =============================================================================
 
 def match_func(*names):
+    """Register a matching function with diferent aliases
 
+    Example:
+
+    ::
+
+        @match_func("fancy_name", "ugly_name")
+        def matching_function(subssn):
+            ...
+
+    Then you can use this this function in you ``before_session_starts`` method
+    as:
+
+    ::
+
+        def before_session_starts(self):
+            self.match_players("fancy_name")
+
+    or
+
+    ::
+
+        def before_session_starts(self):
+            self.match_players("ugly_name")
+
+    """
     def _wrap(func):
 
         @functools.wraps(func)
@@ -46,83 +72,88 @@ def match_func(*names):
 # =============================================================================
 
 def players_x_groups(subssn):
+    """Conver a subssn in a tuple of list of players
+
+    """
     return tuple(g.get_players() for g in subssn.get_groups())
 
 
+# =============================================================================
+# MATCH
+# =============================================================================
+
 @match_func("perfect_strangers", "round_robin")
-def perfect_strangers(subssn):
+def round_robin(subssn):
+    """Try to generate every group of players with a lesser probabilty to mixe
+    the same players twice
 
-    def gen_pxg_id(pxg):
-        groups = []
-        for players in pxg:
-            ply_ids = [ply.id for ply in players]
-            ply_ids.sort()
-            groups.append(tuple(ply_ids))
-        groups.sort()
-        groups_str = [",".join(map(str, g)) for g in groups]
-        return "|".join(groups_str)
+    """
+    # all available players
+    all_players = tuple(
+        itertools.chain.from_iterable(players_x_groups(subssn)))
 
-    def roundrobin(iterable, n):
+    def intersect_any(players, already_choiced):
+        """Return true if players groups already exists in any already_choiced
+        list of groups
 
-        def norepeat(tail, buff):
-            group = []
-            if tail:
-                sg, sub_tail = tuple(sorted(tail[0])), tail[1:]
-                if not buff.intersection(sg):
-                    buff.update(sg)
-                    group.append(sg)
-                group += norepeat(sub_tail, buff)
-            return group
+        """
+        players_set = frozenset(players_ids(players))
+        all_ready_choiced = map(
+            players_ids,
+            [plys for plys in already_choiced if len(plys) == len(players)])
+        return any(map(players_set.intersection, already_choiced))
 
-        subgroups = tuple(itertools.combinations(iterable, n))
-        subgroups_len = len(subgroups)
-        yielded = set()
-        for idx in range(subgroups_len):
-            sg = tuple(sorted(subgroups[idx]))
-            tail = subgroups[:idx] + subgroups[idx + 1:]
-            buff = set(sg)
-            group = [sg] + norepeat(tail, buff)
-            group = tuple(sorted(group))
-            if group not in yielded:
-                yielded.add(group)
-                yield group
+    def players_ids(players):
+        """Generate a unique id as tuple for a group of players"""
+        return tuple(sorted(ply.participant_id for ply in players))
 
-    # retrieve all users groups
-    pxg_ids_cnt = {}
-    pxg_ids_to_pxg = {}
+    # first retrieve all existing groups count in prev rounds groped by size
+    prev_round_buff = collections.defaultdict(
+        lambda: collections.defaultdict(int))
     for p_subssn in subssn.in_previous_rounds():
-        pxg = players_x_groups(p_subssn)
-        pxg_ids = gen_pxg_id(pxg)
-        if pxg_ids in pxg_ids_cnt:
-            pxg_ids_cnt[pxg_ids] += 1
-        else:
-            pxg_ids_cnt[pxg_ids] = 1
-            pxg_ids_to_pxg[pxg_ids] = pxg
+        for players in players_x_groups(p_subssn):
+            size, ply_ids = len(players), players_ids(players)
+            prev_round_buff[size][ply_ids] += 1
 
-    players = tuple(itertools.chain.from_iterable(players_x_groups(subssn)))
+    # this is subroutine for select the less frequent group
+    prev_round_lfreq = {}  # lesser frequency for a given size
+    prev_round_lfreq_players = {}  # the players with the lfreq for given size
+    def get_less_frequent(players, size, already_choiced):
+        """Get the most infrequent combintation of players"""
+        for comb in itertools.combinations(players, size):
+            if not intersect_any(players, already_choiced):
+                ply_ids = players_ids(comb)
+                freq = prev_round_buff[size][ply_ids]
+                if freq == 0:
+                    return players
+                elif size not in prev_round_lfreq:
+                    prev_round_lfreq[size] = freq
+                    prev_round_lfreq_players[size] = comb
+                elif freq < prev_round_lfreq[size]:
+                    prev_round_lfreq[size] = freq
+                    prev_round_lfreq_players[size] = comb
 
-    ppg = subssn._Constants.players_per_group
+        return prev_round_lfreq_players[size]
 
-    for pxg in roundrobin(players, ppg):
-        pxg_ids = gen_pxg_id(pxg)
-        if pxg_ids not in pxg_ids_cnt:
-            return tuple(pxg)
+    groups = []
+    for ppg in subssn._get_players_per_group_list():
+        groups.append(get_less_frequent(all_players, ppg, groups))
 
-    pxg_ids_cnt_items = pxg_ids_cnt.items()
-    pxg_ids_cnt_items.sort(key=lambda e: e[1])
-
-    key = pxg_ids_cnt_items[0][0]
-    return tuple(pxg_ids_to_pxg[key])
+    return tuple(groups)
 
 
 @match_func("partners")
 def partners(subssn):
+    """Every player go with the same player in every round"""
     return players_x_groups(subssn)
 
 
 @match_func("reversed", "players_reversed")
 def players_reversed(subssn):
+    """Change the order of a players ina group. In a even group the central
+    player never change
 
+    """
     def reverse_group(g):
         return list(reversed(g))
 
