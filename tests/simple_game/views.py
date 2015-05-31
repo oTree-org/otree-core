8e1353645b6d2fe92c75313195ae74220572fb23
wickens <c.wickens@gmail.com>
2015-05-31 14:42:34 +0700

    simple_game can now be used to test otree-core features

--- tests/simple_game/views.py
+++ tests/simple_game/views.py
@@ -37,9 +37,9 @@ class MyPage(Page):
         if value % 2:
             return 'Must be an even number'
 
-    #def error_message(self, values):
-    #    if values['add100_1'] + values['add100_2'] != 100:
-    #        return 'The numbers must add up to 100'
+    def error_message(self, values):
+        if values['add100_1'] + values['add100_2'] != 100:
+            return 'The numbers must add up to 100'
 
     def before_next_page(self):
         self.player.after_next_button_field = True
