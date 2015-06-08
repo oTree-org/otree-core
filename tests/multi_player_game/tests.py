00686198e54a62919dbc152aae2d59c741867a86
wickens <c.wickens@gmail.com>
2015-06-09 00:22:02 +0700

    .

---
+++ tests/multi_player_game/tests.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from . import views
+from ._builtin import Bot
+import random
+from otree.common import Currency, currency_range
+from .models import Constants
+
+
+class PlayerBot(Bot):
+
+    def play_round(self):
+
+        self.submit(views.FieldOnOtherPlayer, {'add100_1': 1, 'add100_2': 99})
+        self.submit(views.Results)
+
+    def validate_play(self):
+        pass
