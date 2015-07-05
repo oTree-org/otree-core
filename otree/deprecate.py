098c11bc079932d0f707931119a849091c016f0d
Juan B Cabral <jbc.develop@gmail.com>
2015-07-05 02:47:16 -0300

    added deprecation module

---
+++ otree/deprecate.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import functools
+import warnings
+
+from django.conf import settings
+
+
+# =============================================================================
+# CONSTANTS
+# =============================================================================
+
+OTREE_DEPRECATION_WARNING = getattr(
+    settings, "OTREE_DEPRECATION_WARNING", "default")
+
+MSG_TPL = "Call to deprecated function '{name}'."
+
+MSG_ALTERNATIVE_TPL = (
+    "Call to deprecated function '{name}'. Instead please use '{alternative}'")
+
+
+# =============================================================================
+# DEPRECATED CLASS
+# =============================================================================
+
+class OTreeDeprecationWarning(DeprecationWarning):
+    pass
+
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
+
+def dmessage(func, alternative=None):
+    """Create a simple deprecation message for a function, and if is given
+    suggest an alternative
+
+    """
+    name = func.__name__
+    template = MSG_TPL if alternative is None else MSG_ALTERNATIVE_TPL
+    return template.format(name=name, alternative=alternative)
+
+
+def deprecated(alternative=None):
+    """Mark deprecated functions with this decorator.
+
+    Params
+    ------
+
+    alternative: None or str
+        Show which is the alternative to this call
+
+    .. warning::
+
+        Use it as the closest one to the function you decorate.
+
+    """
+
+    def _decorator(func):
+
+        msg = dmessage(func, alternative)
+
+        @functools.wraps(func)
+        def _wraps(*args, **kwargs):
+            dwarning(msg)
+            return func(*args, **kwargs)
+        return _wraps
+
+    return _decorator
+
+
+def dwarning(msg):
+    """Show a oTree deprecation warning with the given message"""
+    warnings.warn(msg, OTreeDeprecationWarning, stacklevel=2)
+
+
+# =============================================================================
+# CONFIGURATIONS
+# =============================================================================
+
+warnings.simplefilter(OTREE_DEPRECATION_WARNING, OTreeDeprecationWarning)
---
+++ tests/test_deprecate.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import string
+import random
+import warnings
+
+from otree import deprecate
+
+from .base import TestCase
+
+
+class DeprecateTest(TestCase):
+
+    def random_string(self):
+        chars = list(string.letters + string.digits)
+        idx = random.randint(5, len(chars)-1)
+        random.shuffle(chars)
+        return random.choice(string.letters) + "".join(chars[:idx])
+
+    def extract_otree_dwarn(self, l):
+        for w in l:
+            if w.category == deprecate.OTreeDeprecationWarning:
+                return w
+
+    def test_dmessage(self):
+
+        def create_random_object():
+            name = self.random_string()
+            return type(name, (), {}), name
+
+        obj, name = create_random_object()
+        msg = deprecate.MSG_TPL.format(name=name)
+        self.assertEqual(msg, deprecate.dmessage(obj))
+
+        obj, name = create_random_object()
+        alt = self.random_string()
+        msg = deprecate.MSG_ALTERNATIVE_TPL.format(name=name, alternative=alt)
+        self.assertEqual(msg, deprecate.dmessage(obj, alt))
+
+    def test_deprecated(self):
+
+        @deprecate.deprecated()
+        def dfunction():
+            pass
+
+        msg = deprecate.dmessage(dfunction)
+        with warnings.catch_warnings(record=True) as warns:
+            dfunction()
+            dw = self.extract_otree_dwarn(warns)
+            self.assertTrue(dw)
+            self.assertEqual(dw.message.message, msg)
+
+        alternative = self.random_string()
+        @deprecate.deprecated(alternative)
+        def dfunction():
+            pass
+
+        msg = deprecate.dmessage(dfunction, alternative)
+        with warnings.catch_warnings(record=True) as warns:
+            dfunction()
+            dw = self.extract_otree_dwarn(warns)
+            self.assertTrue(dw)
+            self.assertEqual(dw.message.message, msg)
+
+    def test_dwarning(self):
+
+        with warnings.catch_warnings(record=True) as warns:
+            msg = self.random_string()
+            deprecate.dwarning(msg)
+            dw = self.extract_otree_dwarn(warns)
+            self.assertTrue(dw)
+            self.assertEqual(dw.message.message, msg)
+
+        with warnings.catch_warnings(record=True) as warns:
+            warnings.simplefilter("ignore", deprecate.OTreeDeprecationWarning)
+            msg = self.random_string()
+            deprecate.dwarning(msg)
+            dw = self.extract_otree_dwarn(warns)
+            self.assertFalse(dw)
+
+        with warnings.catch_warnings(record=True) as warns:
+            warnings.simplefilter("error", deprecate.OTreeDeprecationWarning)
+            msg = self.random_string()
+            with self.assertRaises(deprecate.OTreeDeprecationWarning) as cm:
+                deprecate.dwarning(msg)
+            self.assertEqual(cm.exception.message, msg)
