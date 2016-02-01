3fa011de9979b9afed7d06e29e0e26b6b8e17301
Gregor Müllegger <gregor@muellegger.de>
2016-02-01 13:02:36 +0100

    Add tests in an attempt to reproduce #429

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import easymoney
+import random
 
 from django.utils import translation
 from django.utils.encoding import force_text
@@ -117,6 +118,12 @@ class SliderInputTests(TestCase):
     maxDiff = None
 
     class SlideInputTestsModel(otree.db.models.Model):
+        def random_invest():
+            return 98
+
+        currencyfield = otree.db.models.CurrencyField(
+            min=0, max=100, default=random_invest,
+            widget=otree.widgets.SliderInput())
         floatfield = otree.db.models.FloatField(
             min=0, max=2.5, default=0,
             widget=otree.widgets.SliderInput(attrs={'step': '0.01'}))
@@ -154,3 +161,44 @@ class SliderInputTests(TestCase):
     def test_sliderinput_with_float_field_and_locale_set(self):
         with translation.override('de-de'):
             self.test_sliderinput_with_float_field()
+
+    def test_with_currencyfield(self):
+        class Form(otree.forms.ModelForm):
+            class Meta:
+                model = self.SlideInputTestsModel
+                fields = ('currencyfield',)
+
+        form = Form()
+        self.assertHTMLEqual(
+            force_text(form['currencyfield']),
+            '''
+            <div class="input-group slider" data-slider>
+                <input
+                    type="range"
+                    name="currencyfield"
+                    value="98"
+                    required
+                    max="100"
+                    step="0.01"
+                    id="id_currencyfield"
+                    min="0"
+                    class="form-control"
+                >
+                <span
+                    class="input-group-addon"
+                    data-slider-value
+                    title="current value"
+                ></span>
+            </div>
+            <input
+                type="hidden"
+                name="initial-currencyfield"
+                value="98"
+                id="initial-id_currencyfield"
+                class="form-control"
+            >
+            ''')
+
+    def test_with_currencyfield_and_locale(self):
+        with translation.override('de-de'):
+            self.test_with_currencyfield()
