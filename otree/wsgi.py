f7333250e4cb6ec380cbe5639f9253099296dd45
wickens <c.wickens@gmail.com>
2016-02-11 15:51:09 +0100

    fixed problem with whitenoise

--- otree/wsgi.py
+++ otree/wsgi.py
@@ -1,9 +1,11 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+
+
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-import os
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
