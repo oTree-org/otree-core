700954b550518fbc217325b17a52f10b38f922fc
Juan B Cabral <jbc.develop@gmail.com>
2016-02-29 17:23:15 -0300

    test pass

--- otree/management/commands/ls_sessions.py
+++ otree/management/commands/ls_sessions.py
@@ -7,22 +7,9 @@
 # =============================================================================
 from __future__ import print_function, unicode_literals
 
-import csv
-import argparse
-import logging
-import itertools
-
 from django.core.management.base import BaseCommand
 
 from otree.models import Session
-
-
-# =============================================================================
-# LOGGER
-# =============================================================================
-
-logger = logging.getLogger('otree')
-
 
 
 # =============================================================================
