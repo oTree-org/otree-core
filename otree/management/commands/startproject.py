71990af06cd2dca0266cadb188ee023a7c485813
Juan B Cabral <jbc.develop@gmail.com>
2016-03-01 13:44:44 -0300

    fix #439

--- otree/management/commands/startproject.py
+++ otree/management/commands/startproject.py
@@ -6,20 +6,36 @@
 # IMPORTS
 # =============================================================================
 
+import sys
 import os
+
 from django.core.management.commands import startproject
+
+import six
+
 import otree
 
-# =============================================================================
-# LOGGER
-# =============================================================================
 
-from django.utils import six
-
+# =============================================================================
+# COMMAND
+# =============================================================================
 
 class Command(startproject.Command):
-    help = (
-        "Creates a new oTree project.")
+    help = ("Creates a new oTree project.")
+
+    def render_runtime(self, options):
+        project_name, target = options['name'], options['directory']
+        if target is None:
+            top_dir = os.path.join(os.getcwd(), project_name)
+        else:
+            top_dir = os.path.abspath(os.path.expanduser(target))
+
+        runtime_path = os.path.join(top_dir, "runtime.txt")
+        version = ".".join(map(str, sys.version_info[:3])) + "\n"
+
+        with open(runtime_path, "w") as fp:
+            fp.write(version)
+
 
     def handle(self, *args, **options):
         answer = None
@@ -39,3 +55,6 @@ class Command(startproject.Command):
         if options.get('template', None) is None:
             options['template'] = location
         super(Command, self).handle(*args, **options)
+
+        self.render_runtime(options)
+
