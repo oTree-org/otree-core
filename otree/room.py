5f3f1b6343e835bd8c7876ab93d77ea260b3b1a7
wickens <c.wickens@gmail.com>
2016-03-28 21:46:55 +0900

    room prototyping

--- otree/room.py
+++ otree/room.py
@@ -1,26 +1,32 @@
 from collections import OrderedDict
 from django.conf import settings
+from otree.models import Session
+from otree.models_concrete import RoomSession
+
+class Room(object):
+
+    def __init__(self, name, participant_label_file=None):
+        self.participant_label_file = participant_label_file
+        self.name = name
+
+    @property
+    def session(self):
+        try:
+            return RoomSession.objects.get(room=self.name)
+        except RoomSession.DoesNotExist:
+            return None
+
+    def has_participant_labels(self):
+        return bool(self.participant_label_file)
+
+    def get_participant_labels(self):
+        if self.has_participant_labels():
+            with open(self.participant_label_file) as f:
+                labels = [line.strip() for line in f if line.strip()]
+                return labels
+        raise Exception('no guestlist')
 
 ROOM_DICT = OrderedDict()
 for room in settings.ROOMS:
-    ROOM_DICT[room['name']] = room
-
-for room_session in RoomSession.objects.all():
-    ROOM_DICT[room_session.room_name]['session']
-
-SESSION_CONFIGS_DICT = OrderedDict()
-for config in settings.SESSION_CONFIGS:
-    SESSION_CONFIGS_DICT[config['name']] = augment_session_config(config)
-
-def get_participant_labels(room_name):
-    room = ROOM_DICT[room_name]
-    # TODO: label file should be optional?
-    label_file_name = room['participant_label_file']
-    with open(label_file_name) as f:
-        labels = [line.strip() for line in f if line.strip()]
-        return labels
-
-def
-
-for room in ROOMS:
+    ROOM_DICT[room['name']] = Room(room['name'], room.get('participant_label_file'))
 
