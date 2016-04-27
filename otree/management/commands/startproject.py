9c3784e190a4dc80d12210f4f3b8ddeb89cf16e5
oTree-org <example@example.example>
2016-04-27 14:50:26 +0900

    startproject should add migrations files to each app

--- otree/management/commands/startproject.py
+++ otree/management/commands/startproject.py
@@ -9,6 +9,8 @@
 import sys
 import os
 import platform
+import string
+import errno
 
 from django.core.management.commands import startproject
 
@@ -25,6 +27,13 @@ IMPLEMENTATIONS_ALIAS = {
     "CPython": "python"
 }
 
+def make_sure_path_exists(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise exception
+
 
 # =============================================================================
 # COMMAND
@@ -36,18 +45,34 @@ class Command(startproject.Command):
     def render_runtime(self, options):
         project_name, target = options['name'], options['directory']
         if target is None:
-            top_dir = os.path.join(os.getcwd(), project_name)
+            project_root_dir = os.path.join(os.getcwd(), project_name)
         else:
-            top_dir = os.path.abspath(os.path.expanduser(target))
+            project_root_dir = os.path.abspath(os.path.expanduser(target))
 
         imp = platform.python_implementation()
         implementation_name = IMPLEMENTATIONS_ALIAS.get(imp, imp).lower()
         version = ".".join(map(str, sys.version_info[:3]))
         runtime_string = "{}-{}\n".format(implementation_name, version)
 
-        runtime_path = os.path.join(top_dir, "runtime.txt")
+        runtime_path = os.path.join(project_root_dir, "runtime.txt")
         with open(runtime_path, "w") as fp:
             fp.write(runtime_string)
+
+        # for each app in the project folder,
+        # add a migrations folder
+        # we do it here instead of modifying the games repo directly,
+        # because people on older versions of oTree also install
+        # from the same repo,
+        # and the old resetdb chokes when it encounters an app with migrations
+        subfolders = next(os.walk(project_root_dir))[1]
+        for subfolder in subfolders:
+            # ignore folders that start with "." or "_" etc...
+            if subfolder[0] in string.ascii_letters:
+                migrations_folder_path = os.path.join(project_root_dir, subfolder, 'migrations')
+                make_sure_path_exists(migrations_folder_path)
+                init_file_path = os.path.join(migrations_folder_path, '__init__.py')
+                with open(init_file_path, 'w') as f:
+                    f.write('')
 
     def handle(self, *args, **options):
         answer = None
