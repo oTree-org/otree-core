df390568a6440ecb002457a917ea605bd68159d9
oTree-org <example@example.example>
2016-06-08 08:32:22 +0900

    runprodserver/webandworkers: delete unnecessary code and consolidate. also, runprodserver should not launch nested honcho processes, seems like it could cause issues. simpler to do it flat. and if someone needs to use a Procfile, they can just use honcho CLI instead of runprodserver.

--- otree/management/commands/runprodserver.py
+++ otree/management/commands/runprodserver.py
@@ -1,43 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-
 from django.core.management import call_command
-from django.core.management.base import BaseCommand
-from django.core.management.base import CommandError
-import honcho.command
-import honcho.manager
+from . import webandworkers
 
 
-class HonchoConifg(object):
-    """Shim for arguments passed into honcho's ``command_start``."""
+class Command(webandworkers.Command):
 
-    # Defaults for honcho
-    app_root = os.path.abspath('.')
-    concurrency = None
-    env = '.env'
-    processes = ()
-    quiet = None
-
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-
-class Command(BaseCommand):
-    help = 'Run otree services for the production environment.'
-
-    default_port = 5000
+    '''
+    It's almost the same as webandworkers,
+    but it also runs collectstatic,
+    and launches the timeoutworker.
+    this command is intended for generic server deployment,
+    whereas webandworkers is intended for Heroku.
+    '''
 
     def add_arguments(self, parser):
-        ahelp = (
-            'The path to the Procfile that should be executed. '
-            'The default is ./Procfile')
-        parser.add_argument(
-            '--procfile', action='store', dest='procfile',
-            default='./Procfile', help=ahelp)
-
+        super(Command, self).add_arguments(parser)
         ahelp = (
             'By default we will collect all static files into the directory '
             'configured in your settings. Disable it with this switch if you '
@@ -46,33 +25,21 @@ class Command(BaseCommand):
             '--no-collectstatic', action='store_false', dest='collectstatic',
             default=True, help=ahelp)
 
-        ahelp = (
-            'The port that the wsgi server should run on. It defaults to '
-            '5000. This value can be set by the environment variable $PORT.')
-        parser.add_argument(
-            '--port', action='store', type=int, dest='port', default=None,
-            help=ahelp)
-
-    def get_port(self, suggested_port):
-        if suggested_port is None:
-            suggested_port = os.environ.get('PORT', None)
-        try:
-            return int(suggested_port)
-        except (ValueError, TypeError):
-            return self.default_port
+    def get_honcho_manager(self, options):
+        manager = super(Command, self).get_honcho_manager(options)
+        manager.add_process(
+            'timeoutworker',
+            'otree timeoutworker',
+            quiet=False,
+            env=self.get_env(options)
+        )
+        return manager
 
     def handle(self, *args, **options):
-        port = self.get_port(options['port'])
-        procfile = options['procfile']
         collectstatic = options['collectstatic']
-
-        args = HonchoConifg(procfile=procfile, port=port)
 
         if collectstatic:
             self.stdout.write('Running collectstatic ...', ending='')
             call_command('collectstatic', interactive=False, verbosity=1)
 
-        try:
-            honcho.command.command_start(args)
-        except honcho.command.CommandError as error:
-            raise CommandError(error)
+        return super(Command, self).handle(*args, **options)
