511af5e5274ee12ccd6aaf2e41af44d3a97c53c6
Bertrand Bordage <bordage.bertrand@gmail.com>
2016-06-21 11:10:01 +0200

    Creates a parallel multi-browser stress test.

--- otree/management/commands/stress_test.py
+++ otree/management/commands/stress_test.py
@@ -4,16 +4,20 @@ from __future__ import unicode_literals, division, print_function
 from collections import OrderedDict, defaultdict
 from fractions import gcd
 from functools import reduce
+from django.conf import settings
+from django.db import connection
 try:
     from http.client import CannotSendRequest, BadStatusLine
 except ImportError:  # Python 2.7
     from httplib import CannotSendRequest, BadStatusLine
 import json
+from math import ceil
 import os
 import platform
 import re
 from socket import socket
 from subprocess import call, Popen, PIPE, STDOUT, CalledProcessError
+from threading import Thread, local
 from time import time, sleep
 import uuid
 
@@ -21,8 +25,9 @@ from django.core.management.base import BaseCommand
 from django.core.urlresolvers import reverse, resolve
 from django.utils.formats import date_format
 from django.utils.timezone import now
+from psutil import Process, NoSuchProcess
 from selenium.common.exceptions import (
-    NoSuchElementException, WebDriverException,
+    NoSuchElementException, NoSuchWindowException
 )
 from selenium.webdriver import Chrome
 from selenium.webdriver.support.select import Select
@@ -133,7 +138,7 @@ class Graph:
     </script>
     """
 
-    def __init__(self, title, subtitle='', x_title='Number of participants',
+    def __init__(self, title, subtitle='', x_title='',
                  y_title='Time (s)', y_unit='s'):
         self.id = uuid.uuid4()
         self.title = title
@@ -188,7 +193,7 @@ class Report:
     """
 
     def __init__(self):
-        self.graphs = []
+        self.graphs = {}
         self.filename = 'stress_test_report.html'
         print('The report will be generated in %s' % self.filename)
 
@@ -216,10 +221,12 @@ class Report:
         return '<dl>%s</dl>' % ''.join(['<dt>%s</dt><dd>%s</dd>' % (k, v)
                                         for k, v in versions.items()])
 
-    def create_graph(self, title, subtitle):
-        graph = Graph(title, subtitle)
-        self.graphs.append(graph)
-        return graph
+    def get_graph(self, title, subtitle):
+        k = (title, subtitle)
+        if k not in self.graphs:
+            graph = Graph(title, subtitle)
+            self.graphs[k] = graph
+        return self.graphs[k]
 
     def generate(self):
         with open(self.filename, 'w') as f:
@@ -227,7 +234,7 @@ class Report:
                 'datetime': date_format(now(), 'DATETIME_FORMAT'),
                 'conditions': self.conditions,
                 'graphs': ''.join([graph.to_html()
-                                   for graph in self.graphs]),
+                                   for graph in self.graphs.values()]),
             })
 
 
@@ -236,22 +243,34 @@ class Browser:
     selenium_driver = Chrome
     # Installed by chromium-chromedriver under Ubuntu 14.04
     executable_path = '/usr/lib/chromium-browser/chromedriver'
+    max_width = 1920
+    max_height = 1080
+
+    def __init__(self, id=None, width=None, height=None, x=None, y=None):
+        self.id = id
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     def start(self):
-        print('Starting web browser...')
         self.selenium = self.selenium_driver(
             executable_path=self.executable_path)
         self.selenium.implicitly_wait(self.timeout)
         self.selenium.set_script_timeout(self.timeout)
         self.selenium.set_page_load_timeout(self.timeout)
 
+        if self.width is not None and self.height is not None:
+            self.selenium.set_window_size(self.width, self.height)
+        if self.x is not None and self.y is not None:
+            self.selenium.set_window_position(self.x, self.y)
+
     def stop(self):
-        print('Stopping web browser...')
         try:
             self.selenium.quit()
-        except (CannotSendRequest, ConnectionResetError):
-            pass  # Occurs when something wrong happens
-                  # in the middle of a request.
+        except (CannotSendRequest, ConnectionError):
+            # Occurs when something wrong happens in the middle of a request.
+            pass
 
     def get(self, url):
         return self.selenium.get(url)
@@ -259,6 +278,9 @@ class Browser:
     @property
     def current_url(self):
         return self.selenium.current_url
+
+    def reset(self):
+        self.selenium.get('about:blank')
 
     def find(self, css_selector):
         return self.selenium.find_element_by_css_selector(css_selector)
@@ -291,6 +313,132 @@ class Browser:
         self.selenium.save_screenshot(filename)
 
 
+class ParallelBrowsers:
+    max_width = 1920
+    max_height = 1080
+    ideal_window_ratio = max_width / max_height
+
+    def __init__(self, amount=1):
+        self.amount = amount
+
+        columns, rows = self.find_best_window_grid()
+        width = self.max_width // columns
+        height = self.max_height // rows
+
+        column = 0
+        row = 0
+        self.browsers = []
+        for i in range(amount):
+            x = column * width
+            y = row * height
+            self.browsers.append(Browser(i, width, height, x, y))
+            column += 1
+            if column == columns:
+                column = 0
+                row += 1
+
+        self.tasks = []
+
+    def find_best_window_grid(self):
+        if self.amount == 0:
+            return 1, 1
+        columns = self.amount
+        ratios = []
+        while columns > 0:
+            rows = ceil(self.amount / columns)
+            width = self.max_width // columns
+            height = self.max_height // rows
+            ratio = abs((width / height) - self.ideal_window_ratio)
+            ratios.append((ratio, columns, rows))
+            columns -= 1
+        ratio, columns, rows = min(ratios, key=lambda t: t[0])
+        return columns, rows
+
+    def start(self):
+        for browser in self.browsers:
+            browser.start()
+
+    def stop(self):
+        while self.busy:
+            sleep(0.001)
+
+        for browser in self.browsers:
+            browser.stop()
+
+    def reset(self):
+        for browser in self.browsers:
+            browser.reset()
+
+    def garbage_collect_tasks(self):
+        for task in self.tasks.copy():
+            if not task.is_alive():
+                self.tasks.remove(task)
+                if task.exception is not None:
+                    raise task.exception
+
+        # FIXME: This closes idle PostgreSQL connections because we face an
+        #        open connection leak. When the leak is fixed, remove this.
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT pg_terminate_backend(pid)
+                FROM pg_stat_activity
+                WHERE usename = %s AND state = 'idle';
+            """, params=(settings.DATABASES['default']['USER'],))
+
+    @property
+    def busy(self):
+        return any(t.is_alive() for t in self.tasks)
+
+    def wait_iteration(self):
+        self.garbage_collect_tasks()
+        sleep(0.001)
+
+    def wait_until_all_available(self):
+        while self.busy:
+            self.wait_iteration()
+
+    def get_task_for(self, browser):
+        for task in self.tasks:
+            if task.browser is browser:
+                return task
+
+    def get_available_browser(self):
+        while True:
+            for browser in self.browsers:
+                task = self.get_task_for(browser)
+                if task is None:
+                    return browser
+            self.wait_iteration()
+
+    def add_task(self, browser, func):
+        task = BrowserTask(browser, func)
+        self.tasks.append(task)
+        task.start()
+
+    def __getitem__(self, item):
+        return self.browsers[item]
+
+
+class BrowserTask(Thread):
+    def __init__(self, browser, func):
+        super(BrowserTask, self).__init__()
+        self.browser = browser
+        self.func = func
+        self.exception = None
+
+    def run(self):
+        try:
+            self.func()
+        except Exception as exception:
+            filename = 'error.png'
+            try:
+                self.browser.save_screenshot(filename.encode())
+                print('Screenshot saved in %s' % filename)
+            except NoSuchWindowException:
+                pass
+            self.exception = exception
+
+
 class Server:
     procfile_path = 'otree/management/commands/stress_test_procfile'
 
@@ -317,8 +465,15 @@ class Server:
 
     def stop(self):
         print('Stopping oTree server...')
-        self.runserver_process.terminate()
-        self.runserver_process.wait(3)
+        process = Process(self.runserver_process.pid)
+        # We kill each subprocess of the server,
+        # the server process will exit once each subprocess exited.
+        for subprocess in process.children(recursive=True):
+            try:
+                subprocess.kill()
+            except NoSuchProcess:
+                pass
+        process.kill()
 
     def get_url(self, view_name, args=None, kwargs=None):
         return self.url + reverse(view_name, args=args, kwargs=kwargs)
@@ -332,20 +487,62 @@ class Bot:
     session_config = None
     num_participants = 1
 
-    def __init__(self, server, browser, report):
+    def __init__(self, server, browsers, report):
         self.server = server
-        self.browser = browser
+        self.browsers = browsers
+        self.thread_local = local()
+        self.browser = None
         if self.session_config is None:
             raise ValueError(
                 'You must fill the class attribute `session_config`.')
-        self.graph = report.create_graph('', self.session_config)
+        self.report = report
+        self.graph = None
+        self.graph_title = ''
+        self.graph_x_title = ''
+        self.graph_variable = lambda: 0
         self.session = None
         self.id_in_session = None
         self.participant = None
         self.player = None
 
+    @property
+    def browser(self):
+        if self.thread_local.browser is None:
+            return self.browsers[0]
+        return self.thread_local.browser
+
+    @browser.setter
+    def browser(self, browser):
+        self.thread_local.browser = browser
+
+    @property
+    def id_in_session(self):
+        return self.thread_local.id_in_session
+
+    @id_in_session.setter
+    def id_in_session(self, id_in_session):
+        self.thread_local.id_in_session = id_in_session
+
+    @property
+    def player(self):
+        return self.thread_local.player
+
+    @player.setter
+    def player(self, player):
+        self.thread_local.player = player
+
+    @property
+    def participant(self):
+        return self.thread_local.participant
+
+    @participant.setter
+    def participant(self, participant):
+        self.thread_local.participant = participant
+
     def time(self, series_name):
-        return self.graph.get_series(series_name).time(self.num_participants)
+        return self.graph.get_series(
+            series_name
+        ).time(self.graph_variable(self))
 
     def get(self, *args, **kwargs):
         return self.browser.get(*args, **kwargs)
@@ -407,13 +604,45 @@ class Bot:
             self.find_link('Create new session')
 
     def set_up(self):
+        self.graph = self.report.get_graph(self.graph_title,
+                                           self.session_config)
+        self.graph.x_title = self.graph_x_title
         self.create_session()
 
     def tear_down(self):
+        self.browsers.reset()
         self.delete_session()
 
-    def run(self, graph_title=''):
-        self.graph.title = graph_title
+    def participate(self, page_number):
+        participants = self.session.get_participants()
+        self.participant = participants.get(id_in_session=self.id_in_session)
+        url = self.server.url + self.participant._start_url()
+        if page_number == 1:
+            with self.time('Participant page 1'):
+                self.get(url)
+        else:
+            self.get(url)
+        # We need to fetch the participant object again since it was modified
+        # by the view.
+        self.participant = Participant.objects.get(pk=self.participant.pk)
+        self.player = self.participant.get_current_player()
+
+    def run_participant(self, browser, id_in_session, page_number, last_page,
+                        test_page_method):
+        self.browser = browser
+        self.id_in_session = id_in_session
+        self.participate(page_number)
+        try:
+            test_page_method()
+        except SkipPage:
+            return
+
+        is_last_page = page_number == last_page
+        page_name = 'finished' if is_last_page else page_number + 1
+        with self.time('Participant page %s' % page_name):
+            self.submit()
+
+    def run(self):
         self.set_up()
 
         test_page_re = re.compile('^test_page_(\d+)$')
@@ -429,21 +658,17 @@ class Bot:
                         test_page_methods[int(page_match.group(1))] = method
 
         # Runs the test_page_1, 2, 3... methods.
-        last_page = max(test_page_methods)
+        last_page = max(test_page_methods) if test_page_methods else 1
         test_pages = sorted(test_page_methods.items(), key=lambda t: t[0])
-        for i, test_page in test_pages:
+        for page_number, test_page_method in test_pages:
             for id_in_session in range(1, self.num_participants + 1):
-                self.id_in_session = id_in_session
-                self.participate()
-                try:
-                    test_page()
-                except SkipPage:
-                    continue
-
-                is_last_page = i == last_page
-                page_name = 'finished' if is_last_page else i + 1
-                with self.time('Participant page %s' % page_name):
-                    self.submit()
+                browser = self.browsers.get_available_browser()
+                self.browsers.add_task(
+                    browser,
+                    lambda: self.run_participant(
+                        browser, id_in_session, page_number, last_page,
+                        test_page_method))
+            self.browsers.wait_until_all_available()
 
         self.tear_down()
 
@@ -480,20 +705,6 @@ class Bot:
         with self.time('Payments page'):
             self.get(url)
 
-    def participate(self):
-        self.get(self.server.get_url('session_start_links',
-                                     (self.session.pk,)))
-        link = self.find_all(
-            'a[href*="/InitializeParticipant/"]')[self.id_in_session - 1]
-        url = link.get_attribute('href')
-        with self.time('Participant page 1'):
-            self.get(url)
-
-        relative_url = url[len(self.server.url):] + '/'
-        self.participant = Participant.objects.get(
-            code=resolve(relative_url).kwargs['participant_code'])
-        self.player = self.participant.get_current_player()
-
 
 class BotRegistry(list):
     def add(self, bot_class):
@@ -506,24 +717,53 @@ bot_registry = BotRegistry()
 
 class StressTest:
     timeit_iterations = 3
+    concurrent_users_steps = 16
     large_sessions_steps = 8
+    large_sessions_concurrent_users = 2
 
     def __init__(self):
         self.report = Report()
 
-        self.browser = Browser()
-        self.browser.start()
+        self.browsers = ParallelBrowsers(0)
 
         self.server = Server()
         self.server.start()
 
-        self.bots = [bot_class(self.server, self.browser, self.report)
+        self.bots = [bot_class(self.server, self.browsers, self.report)
                      for bot_class in bot_registry]
         self.num_participants = lcm([bot.num_participants
                                      for bot in self.bots])
 
+    def test_concurrent_users(self):
+        num_participants = (self.num_participants
+                            * ceil(self.concurrent_users_steps
+                                   / self.num_participants))
+        title = ('oTree speed with concurrent users and %d participants'
+                 % num_participants)
+
+        steps = list(range(1, self.concurrent_users_steps + 1))
+        progress = tqdm(range(steps[-1]), leave=True,
+                        desc='Concurrent users')
+        for concurrent_users in steps:
+            self.browsers = ParallelBrowsers(concurrent_users)
+            self.browsers.start()
+            for bot in self.bots:
+                bot.browsers = self.browsers
+                bot.graph_title = title
+                bot.graph_x_title = 'Number of concurrent users'
+                bot.graph_variable = lambda bot: bot.browsers.amount
+                bot.num_participants = num_participants
+                bot.run()
+                self.report.generate()  # Updates the report on each iteration.
+            self.browsers.stop()
+            progress.update()
+
     def test_large_sessions(self):
-        title = 'oTree speed when creating large sessions'
+        title = ('oTree speed with large sessions and %d concurrent users'
+                 % self.large_sessions_concurrent_users)
+
+        self.browsers = ParallelBrowsers(self.large_sessions_concurrent_users)
+        self.browsers.start()
 
         steps = []
         step = self.num_participants
@@ -532,13 +772,19 @@ class StressTest:
             step *= 2
 
         for bot in self.bots:
+            bot.browsers = self.browsers
+            bot.graph_title = title
+            bot.graph_variable = lambda bot: bot.num_participants
+            bot.graph_x_title = 'Number of participants'
             progress = tqdm(range(steps[-1]), leave=True,
                             desc='Large sessions (%s)' % bot.session_config)
             for num_participants in steps:
                 bot.num_participants = num_participants
-                bot.run(graph_title=title)
+                bot.run()
                 self.report.generate()  # Updates the report on each iteration.
                 progress.update(num_participants - progress.n)
+
+        self.browsers.stop()
 
     def run(self):
         print('Running all tests %d times...' % self.timeit_iterations)
@@ -548,14 +794,12 @@ class StressTest:
             # This is possible since we measure slow operations. On operations
             # shorter than a millisecond, the timeit loop must be inside.
             for _ in range(self.timeit_iterations):
+                self.test_concurrent_users()
                 self.test_large_sessions()
-        except WebDriverException:
-            self.browser.save_screenshot('selenium_error.png')
-            raise
         except BadStatusLine:
             pass  # Occurs when the browser is closed prematurely.
         finally:
-            self.browser.stop()
+            self.browsers.stop()
             self.server.stop()
             notify('Stress test finished!')
 
