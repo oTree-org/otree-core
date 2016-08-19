ea884e60e83571de3ce52605a4fd5e6e505cccbc
oTree-org <chris@otree.org>
2016-08-19 23:39:59 +0900

    bump

--- otree/__init__.py
+++ otree/__init__.py
@@ -9,7 +9,7 @@
 # e.g. users might think that 0.4.5 is newer than 0.4.49
 
 # REMEMBER TO ALSO UPDATE THE PROJECT TEMPLATE
-VERSION = ('0', '9', '0')
+VERSION = ('0', '9', '1')
 
 __version__ = ".".join(VERSION)
 
