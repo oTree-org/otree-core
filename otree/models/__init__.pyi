6683252aea7166b294f0584b989d3779ab9fdbbd
oTree-org <chris@otree.org>
2016-08-24 15:51:27 +0900

    improvements to autocomplete

--- otree/models/__init__.pyi
+++ otree/models/__init__.pyi
@@ -1,12 +1,14 @@
-from typing import List, Union
+from typing import List, Union, Any
 from otree.common import Currency, RealWorldCurrency
+
+
 
 class Session:
 
     config = None  # type: dict
     vars = None  # type: dict
     def get_participants(self) -> List['Participant']: pass
-    def get_subsessions(self) -> List['BaseSubsession']: pass
+    def get_subsessions(self) -> List['Subsession']: pass
 
 class Participant:
 
@@ -16,7 +18,7 @@ class Participant:
     id_in_session = None  # type: int
     payoff = None  # type: Currency
 
-    def get_players(self) -> List['BasePlayer']: pass
+    def get_players(self) -> List['Player']: pass
     def money_to_pay(self) -> RealWorldCurrency: pass
 
 
@@ -25,34 +27,34 @@ class BaseSubsession:
     session = None # type: Session
     round_number = None  # type: int
 
-    def get_groups(self) -> List['BaseGroup']: pass
-    def get_group_matrix(self) -> List[List['BasePlayer']]: pass
+    def get_groups(self) -> List['Group']: pass
+    def get_group_matrix(self) -> List[List['Player']]: pass
     def set_group_matrix(
             self,
-            group_matrix: Union[List[List['BasePlayer']],List[List[int]]]): pass
-    def get_players(self) -> List['BasePlayer']: pass
-    def in_previous_rounds(self) -> List['BaseSubsession']: pass
-    def in_all_rounds(self) -> List['BaseSubsession']: pass
+            group_matrix: Union[List[List['Player']],List[List[int]]]): pass
+    def get_players(self) -> List['Player']: pass
+    def in_previous_rounds(self) -> List['Subsession']: pass
+    def in_all_rounds(self) -> List['Subsession']: pass
     def before_session_starts(self): pass
-    def in_round(self, round_number) -> 'BaseSubsession': pass
-    def in_rounds(self, first, last) -> List['BaseSubsession']: pass
+    def in_round(self, round_number) -> 'Subsession': pass
+    def in_rounds(self, first, last) -> List['Subsession']: pass
     def group_like_round(self, round_number: int): pass
     def group_randomly(self, fixed_id_in_group: bool=False): pass
 
 class BaseGroup:
 
     session = None # type: Session
-    subsession = None  # type: BaseSubsession
+    subsession = None  # type: Subsession
     round_number = None  # type: int
 
-    def set_players(self, players_list: List['BasePlayer']): pass
-    def get_players(self) -> List['BasePlayer']: pass
-    def get_player_by_role(self, role) -> 'BasePlayer': pass
-    def get_player_by_id(self, id_in_group) -> 'BasePlayer': pass
-    def in_previous_rounds(self) -> List['BaseGroup']: pass
-    def in_all_rounds(self) -> List['BaseGroup']: pass
-    def in_round(self, round_number) -> 'BaseGroup': pass
-    def in_rounds(self, first: int, last: int) -> List['BaseGroup']: pass
+    def set_players(self, players_list: List['Player']): pass
+    def get_players(self) -> List['Player']: pass
+    def get_player_by_role(self, role) -> 'Player': pass
+    def get_player_by_id(self, id_in_group) -> 'Player': pass
+    def in_previous_rounds(self) -> List['Group']: pass
+    def in_all_rounds(self) -> List['Group']: pass
+    def in_round(self, round_number) -> 'Group': pass
+    def in_rounds(self, first: int, last: int) -> List['Group']: pass
 
 
 class BasePlayer:
@@ -61,14 +63,18 @@ class BasePlayer:
     payoff = None  # type: Currency
     participant = None  # type: Participant
     session = None # type: Session
-    group = None  # type: BaseGroup
-    subsession = None  # type: BaseSubsession
+    group = None  # type: Group
+    subsession = None  # type: Subsession
     round_number = None  # type: int
 
-    def in_previous_rounds(self) -> List['BasePlayer']: pass
-    def in_all_rounds(self) -> List['BasePlayer']: pass
-    def get_others_in_group(self) -> List['BasePlayer']: pass
-    def get_others_in_subsession(self) -> List['BasePlayer']: pass
+    def in_previous_rounds(self) -> List['Player']: pass
+    def in_all_rounds(self) -> List['Player']: pass
+    def get_others_in_group(self) -> List['Player']: pass
+    def get_others_in_subsession(self) -> List['Player']: pass
     def role(self) -> str: pass
-    def in_round(self, round_number) -> 'BasePlayer': pass
-    def in_rounds(self, first, last) -> List['BasePlayer']: pass
+    def in_round(self, round_number) -> 'Player': pass
+    def in_rounds(self, first, last) -> List['Player']: pass
+
+Subsession = Union[BaseSubsession, Any]
+Group = Union[BaseGroup]
+Player = Union[BasePlayer, Any]
