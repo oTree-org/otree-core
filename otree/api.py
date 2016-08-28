c7c44ed53fd71bb49aa10c1be2610cf93cb9ce7c
oTree-org <chris@otree.org>
2016-08-28 21:34:35 +0900

    better PyCharm autocomplete for widgets and model fields

--- otree/api.py
+++ otree/api.py
@@ -1,6 +1,11 @@
+from importlib import import_module as _import_module
+
 from otree.constants import BaseConstants  # noqa
 from otree.models import BaseSubsession, BaseGroup, BasePlayer  # noqa
 from otree.views import Page, WaitPage  # noqa
 from otree.common import Currency, currency_range, safe_json  # noqa
 from otree.bots import Bot, Submission, SubmissionMustFail  # noqa
-from otree import models, widgets  # noqa
+
+models = _import_module('otree.models')
+widgets = _import_module('otree.models')
+
--- otree/api.pyi
+++ otree/api.pyi
@@ -1,2 +1,73 @@
+# mocking the public API for PyCharm autocomplete.
+# one downside is that PyCharm doesn't seem to fully autocomplete arguments
+# in the .pyi. It gives the yellow pop-up, but doesn't complete what you
+# are typing.
+
 def Submission(PageClass, post_data: dict={}, check_html=True): pass
 def SubmissionMustFail(PageClass, post_data: dict={}, check_html=True): pass
+
+class models:
+
+    def __getattr__(self, item):
+        pass
+
+    class _Field(object):
+        def __init__(
+            self,
+            choices=None,
+            widget=None,
+            initial=None,
+            verbose_name=None,
+            doc='',
+            min=None,
+            max=None,
+            blank=False,
+            null=True,
+            help_text='',
+            **kwargs):
+                pass
+
+    class BooleanField(object):
+        def __init__(self,
+                 choices=None,
+                 widget=None,
+                 initial=None,
+                 verbose_name=None,
+                 doc='',
+                 null=True,
+                 help_text='',
+                 **kwargs):
+            pass
+
+    class CharField(object):
+        def __init__(
+                self,
+                choices=None,
+                widget=None,
+                initial=None,
+                verbose_name=None,
+                doc='',
+                max_length=500,
+                blank=False,
+                null=True,
+                help_text='',
+                **kwargs):
+            pass
+
+    class PositiveIntegerField(_Field): pass
+    class IntegerField(_Field): pass
+    class FloatField(_Field): pass
+    class CurrencyField(_Field): pass
+    class TextField(_Field): pass
+
+
+class widgets:
+    def __getattr__(self, item):
+        pass
+
+    class HiddenInput: pass
+    class CheckboxInput: pass
+    class Select: pass
+    class RadioSelect: pass
+    class RadioSelectHorizontal: pass
+    class SliderInput: pass
