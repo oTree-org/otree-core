4206a91e7d883983c59c240f61a342d93a582c93
oTree-org <chris@otree.org>
2016-09-16 12:37:31 +0200

    botworker should initialize bots one by one, rather than the whole session, because that causes other requests to be blocked for a long time

--- otree/channels/asgi_redis.py
+++ otree/channels/asgi_redis.py
@@ -10,13 +10,19 @@ import redis.exceptions
 import six
 
 REDIS_MSG = (
-    "oTree 0.5+ requires Redis to be installed and running, "
+    "oTree requires Redis to be installed and running, "
     "unless you are using the development server (runserver). "
     "See http://otree.readthedocs.io/en/latest/v0.5.html ."
 )
 
 
 class RedisChannelLayer(asgi_redis.RedisChannelLayer):
+
+    # In SAL experiment, we got 503 "queue full" errors when using ~50
+    # browser bots. This occurred even after i enabled multiple botworkers.
+    def __init__(self, *args, capacity=1000, **kwargs):
+        super(RedisChannelLayer, self).__init__(*args, capacity=capacity, **kwargs)
+
     def receive_many(self, channels, block=False):
         try:
             return super(RedisChannelLayer, self).receive_many(channels, block)
