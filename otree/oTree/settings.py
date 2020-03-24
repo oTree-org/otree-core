a28894fd082093c4e233520fe35008f7fbd7575b
PernilleMatthews <=>
2020-03-24 09:50:24 +0100

    Pushing to add settings.py

---
+++ otree/oTree/settings.py
@@ -0,0 +1,87 @@
+from os import environ
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+)
+
+
+SESSION_CONFIGS = [
+    dict(
+        name='bad_influence',
+        display_name="bad influence",
+        num_demo_participants=5,
+        app_sequence=['bad_influence'],
+    ),
+]
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'da'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'USD'
+USE_POINTS = True
+
+ROOMS = [
+    dict(
+        name='econ101',
+        display_name='Econ 101 class',
+        participant_label_file='_rooms/econ101.txt',
+    ),
+    dict(name='live_demo', display_name='Room for live demo (no participant labels)'),
+]
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+DEMO_PAGE_INTRO_HTML = """
+Here are some oTree games.
+"""
+
+# don't share this with anybody.
+SECRET_KEY = 'dn$$a-!!1z(!%*&p*ad*tqx=pidp84@he0x8^rn_@o$27pov2*'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'otree',
+
+]
+
+# inactive session configs
+# dict(name='trust', display_name="Trust Game", num_demo_participants=2, app_sequence=['trust', 'payment_info']),
+# dict(name='prisoner', display_name="Prisoner's Dilemma", num_demo_participants=2,
+#      app_sequence=['prisoner', 'payment_info']),
+# dict(name='volunteer_dilemma', display_name="Volunteer's Dilemma", num_demo_participants=3,
+#      app_sequence=['volunteer_dilemma', 'payment_info']),
+# dict(name='cournot', display_name="Cournot Competition", num_demo_participants=2, app_sequence=[
+#     'cournot', 'payment_info'
+# ]),
+# dict(name='dictator', display_name="Dictator Game", num_demo_participants=2,
+#      app_sequence=['dictator', 'payment_info']),
+# dict(name='matching_pennies', display_name="Matching Pennies", num_demo_participants=2, app_sequence=[
+#     'matching_pennies',
+# ]),
+# dict(name='traveler_dilemma', display_name="Traveler's Dilemma", num_demo_participants=2,
+#      app_sequence=['traveler_dilemma', 'payment_info']),
+# dict(name='bargaining', display_name="Bargaining Game", num_demo_participants=2,
+#      app_sequence=['bargaining', 'payment_info']),
+# dict(name='common_value_auction', display_name="Common Value Auction", num_demo_participants=3,
+#      app_sequence=['common_value_auction', 'payment_info']),
+# dict(name='bertrand', display_name="Bertrand Competition", num_demo_participants=2, app_sequence=[
+#     'bertrand', 'payment_info'
+# ]),
+# dict(name='public_goods_simple', display_name="Public Goods (simple version from tutorial)",
+#      num_demo_participants=3, app_sequence=['public_goods_simple', 'payment_info']),
+# dict(name='trust_simple', display_name="Trust Game (simple version from tutorial)", num_demo_participants=2,
+#      app_sequence=['trust_simple']),
