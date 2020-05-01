08a213c27fd3695539163931d279dc6ac312f192
Frederik Bruun <frederikabruun@gmail.com>
2020-05-01 10:11:18 +0200

    update

--- otree/oTree/bad_influence/otree_extensions/routing.py
+++ otree/oTree/bad_influence/otree_extensions/routing.py
@@ -1,17 +1,7 @@
 from . import consumers
 from django.conf.urls import re_path
 
-# channel_routing = [
-#     route_class(NetworkVoting, path=NetworkVoting.url_pattern),
-# ]n
-
-# websocket_routes = [
-#             re_path(r'ws/network_voting/(?P<group_pk>[0-9]+)/(?P<player_pk>[0-9]+)$', consumers.NetworkVoting),
-#             re_path(r'ws/chat/(?P<player_pk>[0-9]+)/(?P<group_pk>[0-9]+)$', consumers.ChatConsumer)
-#         ]
-
 websocket_routes = [
     re_path(r'ws/chat/(?P<group_pk>[0-9]+)$', consumers.ChatConsumer),
     re_path(r'ws/network_voting/(?P<player_pk>[0-9]+)/(?P<group_pk>[0-9]+)$', consumers.NetworkVoting),
-    # re_path(r'ws/test_chat/(?P<group_pk>[0-9+]$)', consumers.ChatTestConsumer)
 ]
